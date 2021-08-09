8f6caa40567f26db5b56bb34cbf5a8a46f642e54
Joseph Chen <joseph.wc.chen@gmail.com>
2021-08-09 16:39:54 +0800

    add new file

---
+++ c_to_f.py
@@ -0,0 +1,4 @@
+c = input('please input current temperature in celsius: ') #string
+c = float(c) # casting from string to float
+f = c * (9 / 5) + 32
+print('current temperature in Fahrenheit is:', f)
